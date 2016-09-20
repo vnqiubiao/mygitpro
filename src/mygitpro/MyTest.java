59083d5caa147c51f9562ec8d34945cd5435bdb7
qiubiao <qiu8360336@qq.com>
2016-09-20 14:10:29 +0800

    test some function.

--- src/mygitpro/MyTest.java
+++ src/mygitpro/MyTest.java
@@ -4,6 +4,8 @@ public class MyTest {
 
 	public static void main(String[] args) {
 		System.out.println("I am starting to use git & sourcetree.");
+		
+		System.out.println("now i want to test some function.");
 
 	}
 
